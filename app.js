d19265e91d221c13a96ee946136e8c178336c051
zami <alzamiarafat00@gmail.com>
2021-01-16 22:56:24 +0600

    MongoDB setup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -6,4 +7,8 @@ app.get('/', (req,res) =>{
 	res.send('hello world');
 });
 
+mongoose.connect('mongodb+srv://zami:zami123456@cluster0.2b1je.mongodb.net/zami?retryWrites=true&w=majority',
+		{ useNewUrlParser: true , useUnifiedTopology: true },
+		() =>{ console.log('Connect to db');}
+	);
 app.listen(5000);
